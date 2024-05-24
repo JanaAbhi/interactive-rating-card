9ab1ea5bae202872d0d991073c1883fbef3223de
Jana <113575455+JanaAbhi@users.noreply.github.com>
2024-05-24 13:12:18 +0530

    adding condition before submit

--- action.js
+++ action.js
@@ -1,17 +1,25 @@
-let container= document.querySelector(".container");
-let thankState=document.querySelector(".thank__state");
-const submit =document.querySelector(".btn--submit");
-const rates=document.getElementById("rating");
-const rateNumber=document.querySelectorAll(".btn--class");
+let container = document.querySelector(".container");
+let thankState = document.querySelector(".thank__state");
+const submit = document.querySelector(".btn--submit");
+const rates = document.getElementById("rating");
+const rateNumber = document.querySelectorAll(".btn--class");
+let selectedRating = null;
 
-submit.addEventListener("click",()=>{
+
+submit.addEventListener("click", () => {
+  if (selectedRating) {
     thankState.classList.remove("hidden--state");
-    container.style.display="none"
-})
+    container.style.display = "none";
+  }else{
+    alert("Please select a rating before submitting.")
+  }
+});
 
-rateNumber.forEach((e)=>{
- e.addEventListener("click",()=>{
-    rates.innerHTML=e.innerHTML;
-    console.log(e.innerHTML);
- })
-})
+rateNumber.forEach((e) => {
+    e.addEventListener("click", () => {
+      selectedRating=e.innerHTML;
+      rates.innerHTML = selectedRating;
+      console.log(selectedRating);
+    });
+  });
+
